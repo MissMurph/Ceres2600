7286425dbefcf1926a23dd67a4de1f4c8f09c7d6
Murphy Morgan <murphyhmorgan@gmail.com>
2023-07-27 13:39:40 +1000

    Successful Ragdoll
    Had personal setback meaning I couldn't reliably work, been doing heaps of experimenting & practicing with Bodies, Rigging & Modelling to make progress towards gibbing. Been a lot of trial and error, however we FINALLY have a reliable, good looking ragdoll set up. Basic character model is ready & rigged up but with no animations.
    
    This commit is mostly about learning, I've learnt a lot more about the workflow between Blender & Unity, let alone sculpting, texturing and rigging models.

--- Assets/Entities/Bodies/BodyPart.cs
+++ Assets/Entities/Bodies/BodyPart.cs
@@ -7,17 +7,30 @@ namespace Ceres.Entities {
 	public class BodyPart : MonoBehaviour {
 
 		private Body parentBody;
-		private Rigidbody localBody;
+		private Rigidbody localPhysics;
+
+		[SerializeField]
+		private Rigidbody[] giblets;
+
+		[SerializeField]
+		private float gibEjectForce;
 
 		private void Awake () {
 			parentBody = GetComponentInParent<Body>();
-			localBody = GetComponent<Rigidbody>();
+			localPhysics = GetComponent<Rigidbody>();
 		}
 
 		public void Attack (Vector3 hit, int damage) {
+			System.Random rando = new System.Random();
 			//We first need to determine if this is a killing hit
 			if (parentBody.Health - damage <= 0) {
+				foreach (Rigidbody gib in giblets) {
+					gib.gameObject.SetActive(true);
+					gib.AddForce(new Vector3(rando.Next(0,100), rando.Next(0, 100), rando.Next(0, 100)).normalized * gibEjectForce, ForceMode.Impulse);
+				}
 
+				parentBody.Health -= damage;
+				Destroy(gameObject);
 			}
 		}
 	}
